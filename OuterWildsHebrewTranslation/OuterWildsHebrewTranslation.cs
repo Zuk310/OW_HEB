eec47ef725b0e4fb5aa63c6d52cfa7ea600ea744
Nick <nicholasconnors@hotmail.ca>
2023-04-10 16:50:05 -0400

    Fix formatting

--- OuterWildsHebrewTranslation/OuterWildsHebrewTranslation.cs
+++ OuterWildsHebrewTranslation/OuterWildsHebrewTranslation.cs
@@ -7,25 +7,25 @@ using System;
 
 namespace OuterWildsHebrewTranslation
 {
-    public class OuterWildsHebrewTranslation : ModBehaviour
-    {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
+	public class OuterWildsHebrewTranslation : ModBehaviour
+	{
+		private void Awake()
+		{
+			// You won't be able to access OWML's mod helper in Awake.
+			// So you probably don't want to do anything here.
+			// Use Start() instead.
+		}
 
-        private void Start()
-        {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"Hebrew translation was loaded successfully!", MessageType.Success);
+		private void Start()
+		{
+			// Starting here, you'll have access to OWML's mod helper.
+			ModHelper.Console.WriteLine($"Hebrew translation was loaded successfully!", MessageType.Success);
 
-            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
-            api.RegisterLanguage(this, "Hebrew", "assets/Translation.xml");
-            api.AddLanguageFont(this, "Hebrew", "assets/hebrewbundle", "Assets/Rubik-Regular.ttf");
+			var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+			api.RegisterLanguage(this, "Hebrew", "assets/Translation.xml");
+			api.AddLanguageFont(this, "Hebrew", "assets/hebrewbundle", "Assets/Rubik-Regular.ttf");
 			api.AddLanguageFixer("Hebrew", Fix);
-        }
+		}
 
 		public string Fix(string s)
 		{
